7c467af67e12bbad01a43f0df0f8ad7c638b51ff
Murat Ozden <muratzden@gmail.com>
2026-07-01 22:58:14 +0300

    feat: add terminal logo to version command

--- commands/version.ps1
+++ commands/version.ps1
@@ -11,7 +11,27 @@ function Invoke-MDWVersion {
         [string[]] $Arguments
     )
 
-    $version = "0.1.1-alpha"
+    $version = '0.1.1-alpha'
+    $toolkitRoot = Get-MDWRootPath
+    $workspaceRoot = Split-Path $toolkitRoot -Parent
+    $powerShellVersion = $PSVersionTable.PSVersion.ToString()
+    $githubUrl = 'https://github.com/muratzden/mdw-toolkit'
+    $logoPath = Join-Path $toolkitRoot 'assets\logo-terminal.txt'
 
-    Write-Host "MDW Toolkit $version"
+    Write-Host ''
+
+    if (Test-Path $logoPath) {
+        Get-Content -Path $logoPath | ForEach-Object {
+            Write-Host $_ -ForegroundColor Cyan
+        }
+
+        Write-Host ''
+    }
+
+    Write-Host "Version      : $version" -ForegroundColor Green
+    Write-Host "PowerShell   : $powerShellVersion"
+    Write-Host "Workspace    : $workspaceRoot"
+    Write-Host "Toolkit      : $toolkitRoot"
+    Write-Host "GitHub       : $githubUrl"
+    Write-Host ''
 }
